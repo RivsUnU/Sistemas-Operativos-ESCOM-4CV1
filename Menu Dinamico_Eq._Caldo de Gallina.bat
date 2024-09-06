0448025673240cda237ed4c3cc3e4b8b6cafd0db
RivsUnU <79726747+RivsUnU@users.noreply.github.com>
2024-09-06 15:38:31 -0600

    Add files via upload

---
+++ Menu Dinamico_Eq._Caldo de Gallina.bat
@@ -0,0 +1,219 @@
+Nombre: Caldo de Gallina.
+Version: 2.0.0
+Fecha: 6/Septiembre/2024
+Explicación:
+
+-Lenguaje fach .bad-
+
+
+@echo off
+setlocal enabledelayedexpansion
+title The Megament
+color 0c
+:menu
+cls
+echo ****************************************
+echo *        Bienvenido al Menu            *
+echo ****************************************
+echo.
+echo 1. Jugar a adivinar tu numero
+echo 2. Cuestionario
+echo 3. Generador Random
+echo 4. Multiplica tus valores
+echo 5. Resta tus valores
+echo 6. Suma tus valores
+echo 7. Salir
+echo.
+set /p "opcion=Elige una opcion: "
+
+if "%opcion%"=="1" goto adivinacion
+if "%opcion%"=="2" goto cuest
+if "%opcion%"=="3" goto rand
+if "%opcion%"=="4" goto mult
+if "%opcion%"=="5" goto rest
+if "%opcion%"=="6" goto sum
+if "%opcion%"=="7" goto salir
+
+:adivinacion
+  cls
+  color 02
+  rem Pide al usuario que ingrese un numero
+  set /p "numero=Piensa en un numero lo ADIVINARE: "
+
+  rem Entrando a sistema Neuronal
+  echo.
+  echo Entrando a la mente...
+  pause >nul  rem Espera a que el usuario presione Enter
+
+  echo Siguiendo tu red neuronal...
+  pause >nul  rem Espera a que el usuario presione Enter
+
+  echo Leyendo mente...
+  pause >nul  rem Espera a que el usuario presione Enter
+
+  echo Procesando tus pensamientos...
+  pause >nul  rem Espera a que el usuario presione Enter
+
+  echo Adivinando tu numero...
+  pause >nul  rem Espera a que el usuario presione Enter
+
+  rem Mensaje final mostrando el número pensado
+  echo.
+  echo El numero que pensaste es: %numero%
+  echo.
+  set /p "respuesta=Es el numero? (si/no): "
+
+  if /i "%respuesta%"=="si" goto menu
+  if /i "%respuesta%"=="no" goto imagen
+
+:imagen
+  rem Si el número no es correcto, redirige a una URL con una imagen
+  start https://www.youtube.com/watch?v=l7ghKFxNknQ rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GBSA2xQXMAA1b-V?format=jpg&name=360x360 rem Abre la imagen en el navegador
+  start https://pbs.twimg.com/media/GMNbeLhX0AAFIDz.jpg:large rem Abre la imagen en el navegador
+
+  goto salir
+
+:cuest
+  cls
+  echo -- Quieres jugar a un juego? --
+  set /p validar="Si/No: "
+  if "%validar%" == "Si" goto cuest_1
+  if "%validar%" == "No" goto finish
+  if not "%validar%" == "No" goto cuest
+  if not "%validar%" == "Si" goto cuest
+
+:cuest_1
+   cls
+   msg * "Con cuidado porque si te equivocas puedo apagarte la compu"
+   msg * "Aceptas el riesgo?"
+   set /p validar_2="Si/No:"
+   if "%validar_2%" == "Si" goto cuest_2
+   if "%validar%" == "No" goto cuest
+   if not "%validar_2%" == "Si" goto cuest_2
+   if not "%validar%" == "No" goto cuest_2
+
+
+:cuest_2
+    cls
+    echo -- Tu compu esta programada para que en 5 min se apague sola --
+    echo.
+    echo -- Hay una contraseña para detenerla, pero no sera tan facil.
+    echo.
+    echo -- Solo tu podras desactivarla --
+    echo.
+    shutdown -s -t 300 -c "Comienza el juego"
+    pause
+    goto cuest_3
+
+:cuest_3
+  cls
+  echo -- Que extension de archivo puede ayudarte a programar en el shell? --
+  set /p validar_3="":
+  if "%validar_3%" == ".bat" msg * "Muy bien, entonces ve creando un txt que guardaras como .bat para poner el codigo"
+  if not "%validar_3%" == ".bat" msg * "Mal, pero como pista ahora mismo estas usando esa extension para correr este programa"
+  if not "%validar_3%" == ".bat" goto cuest_3
+  pause
+  goto cuest_4
+
+:cuest_4
+  cls
+  echo -- Ahora, como decimos apagar en ingles? --
+  set /p validar_4="":
+  if "%validar_4%" == "shutdown" msg * "Bien, ahora en el txt coloca este comando shutdown -a"
+  if not "%validar_4%" == "shutdown" goto cuest_4
+  pause
+  goto start
+
+:rand
+     cls
+     echo -- Generador de numeros aleatorios --
+     set /p min="Ingresa el numero minimo: "
+     set /p max="Ingresa el numero maximo: "
+     set /a numAleatorio=%random% %% (%max% - %min% + 1) + %min%
+     echo El numero aleatorio generado es: %numAleatorio%
+     pause
+     goto start
+        cls
+
+:mult
+    color 0e
+    echo.
+    cls
+    echo ---- Multiplicacion ----
+    rem Solicita al usuario el primer numero a multiplicar
+    set /p mult1="Ingresa el primer numero a multiplicar: "
+
+    rem Solicita el segundo numero a multiplicar
+    set /p mult2="Ingresa el segundo numero a multiplicar: "
+    rem Se realiza la operacion correspondiente
+    set /a resultado = %mult1% * %mult2%
+    rem Muestra al usuario el resultado de la operacion
+    echo El resultado de multiplicar %mult1% por %mult2% es de %resultado%
+   pause
+   goto start
+
+:rest
+  color 0e
+  echo.
+  cls
+  echo ---- Resta ----
+  rem Solicita al usuario ingresar el primer numero a restar
+  set /p res1="Ingresa el primer numero a restar: "
+
+  rem Solicita el segundo numero
+  set /p res2="Ingresar el segundo numero a restar: "
+  rem Se realiza el proceso de la resta
+  set /a resultado = %res1% - %res2%
+  rem Muestra en la pantalla el resultado al usuario
+  echo El resultado de restar %res1% menos %res2% es: %resultado%
+   color 0e
+   pause
+   goto start
+
+:sum
+  cls
+  echo -- Suma --
+  set /p number1="Primer numero: "
+  set /p number2="Segundo numero: "
+  set /a sum=%number1% + %number2%
+  echo El resultado es: %sum%
+  pause
+  goto start
+
+:salir
+msg * Te estoy observando...
+pause
+exit
